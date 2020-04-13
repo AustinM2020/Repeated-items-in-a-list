7708545e1ca033759531df3e407bb120fc27f043
Austin Moen <moenaustin20@gmail.com>
2020-04-13 11:48:14 -0500

    working version of program

---
+++ LinqProblem2/NameLibrary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqProblem2
+{
+    class NameLibrary
+    {
+        public List<string> GetStrings()
+        {
+            return new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike", "Ian", "King"};
+        }
+    }
+}
---
+++ LinqProblem2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProblem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameLibrary nameLibrary = new NameLibrary();
+            List<string> words = nameLibrary.GetStrings();
+
+            var nameList = words.GroupBy(l => l);
+            var duplicates = nameList.Where(d => d.Count() >= 1);
+            var names = duplicates.Select(n => n.Key).ToList();
+
+            foreach(var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
